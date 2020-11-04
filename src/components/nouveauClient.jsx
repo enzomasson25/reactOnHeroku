bda681035650d2c678ee81b04fddf9c9a0c10e4b
enzo masson <enzo_2510@hotmail.fr>
2020-11-04 11:37:09 +0100

    Update nouveauClient.jsx

--- src/components/nouveauClient.jsx
+++ src/components/nouveauClient.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
-const NouveauClient = ()=>(
+const useStyles = makeStyles((theme) => ({
+    root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+      height: '55px',
+    }
+  },
+}));
+
+
+
+
+export default function NouveauClient() {
+  const classes = useStyles();
+  const [nom, setNom] = React.useState("");
+  const [prenom, setPrenom] = React.useState("");
+  const [telephone, setTelephone] = React.useState("");
+
+  const handleSubmit = (event) => {
+    var user = {
+        nom: nom,
+        prenom: prenom,
+        telephone: telephone
+    }
+    
+    console.log(user);
+    event.preventDefault();
+
+    axios.post('https://morning-ravine-89608.herokuapp.com/user', user)
+      .then((response) => {
+        console.log(response);
+      }, (error) => {
+        console.log(error);
+      });
+  }
+
+  return (
     <div>
         <p>Pour les nouveaux clients :</p>
-        
-    </div>
-)
-
-export default NouveauClient;
+        <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+            <TextField id="outlined-basic" label="Nom" id="nom" variant="outlined" required onChange={e => setNom(e.target.value)}/>
+            <TextField id="outlined-basic" label="Prénom" id="prenom" variant="outlined" required onChange={e => setPrenom(e.target.value)}/>
+            <TextField id="outlined-basic" label="Téléphone" id="telephone" variant="outlined" required onChange={e => setTelephone(e.target.value)}/>
+            <Button variant="contained" type="submit" color="primary">
+                Valider
+            </Button>
+        </form>
+    </div> 
+  );
+}
